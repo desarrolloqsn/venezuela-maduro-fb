9f0b8d293e7d0ece678e94f138c28c57fbb35cd7
desarrolloqsn <desarrollo@qsocialnow.com>
2024-04-12 15:11:22 -0300

    dashboard

--- src/contenedores/Dashboard/Filtros.jsx
+++ src/contenedores/Dashboard/Filtros.jsx
@@ -538,7 +538,7 @@ const disabledDate = current => {
 
   return (
     <div>
-    <div className='nombreDashboard'>Francisco Olvera - FB {modeloSinEspacios ? `- ${modeloSinEspacios}` : null}</div>
+    <div className='nombreDashboard'>Dashboard- Maduro - FB {modeloSinEspacios ? `- ${modeloSinEspacios}` : null}</div>
     <div className='contenedor-filtros'>
      <div className='boton-informe'>
      <Tooltip placement="top" title='Generar informe' >
